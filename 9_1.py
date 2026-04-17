abc06c9b51a520becfbe748e6d88df5775255d76
Blqsterz <lavrfell@mail.ru>
2026-04-17 21:28:23 +0300

    second commit

--- 9_1.py
+++ 9_1.py
@@ -6,8 +6,8 @@ img = Image.open(Path(__file__).parent / "picture.jpg")
 img.show()
 
 height, weight = img.size
-color_mode = img.mode
 
 print(f"Размер = {height} x {weight}")
-print(f"Цветовой режим: {color_mode}")
+print(f"Цветовой режим: {img.mode}")
+print(f"Формат изображения:{img.format}")
 
